a087894ffc41a2569868d702dcc7e00ddf65e35b
swo <swo@mit.edu>
2016-04-27 16:27:15 -0400

    markdown test: fixed?
    There's a broader problem: how do you evaluate the equality of picts?

--- markdown-test.rkt
+++ markdown-test.rkt
@@ -5,7 +5,8 @@
          "markdown.rkt")
 
 (check-equal? (parse-markdown-string "hi *there* bob")
-              (list (t "hi") (it "there") (t "bob")) "easy string")
+              (list (t "hi") (it "there") (t "bob"))
+              "easy string")
 
 (check-equal? (parse-markdown-string #:state 'bold "hi *there* bob")
               (list (bt "hi") (bit "there") (bt "bob"))
